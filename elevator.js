c988795c4ef4e469565d40189582e601800f776a
Paul Grayson <pdg@alum.mit.edu>
2015-03-05 01:22:53 -0800

    completed challenge 4 (barely)

--- elevator.js
+++ elevator.js
@@ -1,20 +1,18 @@
 {
   init: function(elevators, floors) {
-    var elevator = elevators[0];
-
-    goToFloorNum = function(floorNum) {
+    goToFloorNum = function(elevator, floorNum) {
       elevator.goToFloor(floorNum)
       setIndicators(elevator)
     }
 
-    goToFloorNumUrgent = function(floorNum) {
+    goToFloorNumUrgent = function(elevator, floorNum) {
       elevator.goToFloor(floorNum,true)
       setIndicators(elevator)
     }
 
-    goToFloorFunction = function(floor) {
+    goToFloorFunction = function(elevator, floor) {
       return function() {
-        goToFloorNum(floor.floorNum())
+        goToFloorNum(elevator, floor.floorNum())
       }
     }
     upPressedFunction = function(floor) {
@@ -59,37 +57,53 @@
       floor.on("down_button_pressed", downPressedFunction(floor))
     }
 
-    elevator.on("passing_floor", function(floorNum) {
-      if(elevator.goingUpIndicator() && upPressed[floorNum] ||
-         elevator.goingDownIndicator() && downPressed[floorNum] ||
-         elevator.getPressedFloors().indexOf(floorNum) != -1)
-        goToFloorNumUrgent(floorNum)
-    } )
-
-    elevator.on("stopped_at_floor", function(floorNum) {
-      setIndicators(elevator)
-      if(elevator.goingUpIndicator())
-        upPressed[floorNum] = false
-      if(elevator.goingDownIndicator())
-        downPressed[floorNum] = false
-    } )
-
-    elevator.on("idle", function() {
-      if(elevator.getPressedFloors().length > 0) {
-        goToFloorNum(elevator.getPressedFloors()[0])
-        return
+    elevatorPassingFloorFunction = function(elevator) {
+      return function(floorNum) {
+        if(elevator.goingUpIndicator() && upPressed[floorNum] ||
+           elevator.goingDownIndicator() && downPressed[floorNum] ||
+           elevator.getPressedFloors().indexOf(floorNum) != -1)
+          goToFloorNumUrgent(elevator,floorNum)
       }
-      for(var i = 0 ; i < floors.length; i++) {
-        if(upPressed[i]) {
-          goToFloorNum(i);
-          break;
-        }
-        if(downPressed[i]) {
-          goToFloorNum(i);
-          break;
-        }
+    }
+
+    elevatorStoppedFunction = function(elevator) {
+      return function(floorNum) {
+        setIndicators(elevator)
+        if(elevator.goingUpIndicator())
+          upPressed[floorNum] = false
+        if(elevator.goingDownIndicator())
+          downPressed[floorNum] = false
       }
-    });
+    }
+
+    elevatorIdleFunction = function(elevator) {
+      return function() {
+
+        if(elevator.getPressedFloors().length > 0) {
+          goToFloorNum(elevator, elevator.getPressedFloors()[0])
+          return
+        }
+
+        for(var i = 0 ; i < floors.length; i++) {
+          if(upPressed[i]) {
+            goToFloorNum(elevator, i);
+            return
+          }
+          if(downPressed[i]) {
+            goToFloorNum(elevator, i);
+            return
+          }
+        }
+
+      }
+    }
+
+    for(var i = 0; i < elevators.length; i++) {
+      elevator = elevators[i]
+      elevator.on("passing_floor", elevatorPassingFloorFunction(elevator))
+      elevator.on("stopped_at_floor", elevatorStoppedFunction(elevator))
+      elevator.on("idle", elevatorIdleFunction(elevator))
+    }
   },
   update: function(dt, elevators, floors) {
   }
